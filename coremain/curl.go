e1860ad7c8907603ef99efc9cb14b57f04f21c13
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-11-11 20:11:09 +0800

    Update curl.go

--- coremain/curl.go
+++ coremain/curl.go
@@ -7,24 +7,43 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"golang.org/x/net/proxy"
 )
 
 func Curl(args []string) {
-	url := args[0]
-	output := args[1]
-	socks5 := args[2]
-	if url == "" {
-		fmt.Println("Please provide a URL using url parameter.")
-		os.Exit(1)
+	var (
+		url    string
+		output string
+		socks5 string
+	)
+
+	if len(args) > 0 {
+		url = args[0]
+	}
+
+	if len(args) > 1 {
+		if strings.Contains(args[1], ":") {
+			socks5 = args[1]
+		} else {
+			output = args[1]
+		}
+	}
+
+	if len(args) > 2 {
+		if strings.Contains(args[2], ":") {
+			socks5 = args[2]
+		} else {
+			output = args[2]
+		}
 	}
 
 	client := &http.Client{
 		Transport: &http.Transport{
 			Dial: func(network, addr string) (net.Conn, error) {
-				if socks5 != "no" {
+				if socks5 != "" {
 					dialer, err := proxy.SOCKS5("tcp", socks5, nil, &net.Dialer{
 						Timeout:   10 * time.Second,
 						DualStack: false,
@@ -62,7 +81,7 @@ func Curl(args []string) {
 	defer resp.Body.Close()
 
 	var outFile io.Writer
-	if output != "s" {
+	if output != "" {
 		file, err := os.Create(output)
 		if err != nil {
 			fmt.Println("Error creating output file:", err)
