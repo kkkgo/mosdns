507d76a93aa493af413ab0645c6b61eda444e323
kissshot <4530897+kkkgo@users.noreply.github.com>
2024-02-28 16:17:08 +0800

    add lite mode

--- plugin/executable/shuffle/shuffle.go
+++ plugin/executable/shuffle/shuffle.go
@@ -1,69 +1,85 @@
-// PaoPaoDNS shuffle
+// PaoPaoDNS shuffle with lite mode
 
 package shuffle
 
 import (
-	"context"
-	"math/rand"
-
-	"github.com/IrineSistiana/mosdns/v5/pkg/query_context"
-	"github.com/IrineSistiana/mosdns/v5/plugin/executable/sequence"
-	"github.com/miekg/dns"
+    "context"
+    "math/rand"
+    "github.com/IrineSistiana/mosdns/v5/pkg/query_context"
+    "github.com/IrineSistiana/mosdns/v5/plugin/executable/sequence"
+    "github.com/miekg/dns"
 )
 
 const (
-	PluginType = "shuffle"
+    PluginType = "shuffle"
 )
 
 func init() {
-	sequence.MustRegExecQuickSetup(PluginType, QuickSetup)
+    sequence.MustRegExecQuickSetup(PluginType, QuickSetup)
 }
-
-var _ sequence.Executable = (*Shuffle)(nil)
 
 type Shuffle struct {
+    lite bool
 }
 
-func NewShuffle() *Shuffle {
-	return &Shuffle{}
+func NewShuffle(lite bool) *Shuffle {
+    return &Shuffle{lite: lite}
 }
 
 func QuickSetup(_ sequence.BQ, s string) (interface{}, error) {
-	return NewShuffle(), nil
+    lite := s == "lite"
+    return NewShuffle(lite), nil
 }
 
 func (s *Shuffle) Exec(_ context.Context, qCtx *query_context.Context) error {
-	response := qCtx.R()
-	if response == nil || response.Answer == nil {
-		return nil
-	}
+    response := qCtx.R()
+    request := qCtx.Q()
 
-	ShuffleDNSAnswers(response)
-	MoveCNAMEToFirst(response)
-	return nil
+    if response == nil || response.Answer == nil {
+        return nil
+    }
+
+    if s.lite {
+        filteredAnswers :=  FilterType(response.Answer, request.Question[0].Qtype)
+         Shuffle(filteredAnswers)
+        response.Answer = filteredAnswers
+    } else {
+         ShuffleSkipCNAME(response.Answer)
+    }
+
+    return nil
 }
 
-func ShuffleDNSAnswers(response *dns.Msg) {
-	answers := response.Answer
-	n := len(answers)
-	for i := n - 1; i > 0; i-- {
-		j := rand.Intn(i + 1)
-		answers[i], answers[j] = answers[j], answers[i]
-	}
+func  FilterType(answers []dns.RR, qtype uint16) []dns.RR {
+    var filtered []dns.RR
+    for _, answer := range answers {
+        if answer.Header().Rrtype == qtype {
+            filtered = append(filtered, answer)
+        }
+    }
+    return filtered
 }
 
-func MoveCNAMEToFirst(response *dns.Msg) {
-	answers := response.Answer
-	cnameRecords := make([]dns.RR, 0)
-	nonCnameRecords := make([]dns.RR, 0)
+func  Shuffle(answers []dns.RR) {
+    n := len(answers)
+    for i := n - 1; i > 0; i-- {
+        j := rand.Intn(i + 1)
+        answers[i], answers[j] = answers[j], answers[i]
+    }
+}
 
-	for _, record := range answers {
-		if _, isCNAME := record.(*dns.CNAME); isCNAME {
-			cnameRecords = append(cnameRecords, record)
-		} else {
-			nonCnameRecords = append(nonCnameRecords, record)
-		}
-	}
-	answers = append(cnameRecords, nonCnameRecords...)
-	response.Answer = answers
+func  ShuffleSkipCNAME(answers []dns.RR) {
+    n := len(answers)
+    for i := 0; i < n; i++ {
+        if _, isCNAME := answers[i].(*dns.CNAME); isCNAME {
+            continue
+        }
+        for j := i + 1; j < n; j++ {
+            if _, isCNAME := answers[j].(*dns.CNAME); !isCNAME {
+                randIndex := rand.Intn(j-i+1) + i
+                answers[i], answers[randIndex] = answers[randIndex], answers[i]
+                break
+            }
+        }
+    }
 }
